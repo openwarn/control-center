74bb7d5ade07d1206566715400e5b789002c9e8c
eisverticker <eisverticker@posteo.de>
2019-02-04 13:55:33 +0100

    Testing: Fix karma freeze with ChromeHeadless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+      HeadlessChrome:{
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
