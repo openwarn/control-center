e78f7dbad41a7831903d8f9ccbcc25c828ca5cbe
eisverticker <eisverticker@posteo.de>
2019-02-20 19:43:59 +0100

    WarningFeed: Use preferred language

--- src/app/warning-feed/warning-feed.component.ts
+++ src/app/warning-feed/warning-feed.component.ts
@@ -6,8 +6,31 @@ import { PendingWarningService } from '../cap/pending-warning.service';
 import * as moment from 'moment';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 moment.locale(environment.locale);
+
+function orderByPreferredLanguages(preferedLanguages: Array<string>) {
+  return pipe(
+    map<CapAlert[], any>(
+      (items) => items.map(
+        (alert) => {
+          const orderedinfoBlocks = alert.alertInfos.sort(
+            (a, b) => {
+              // Sort elements in a way, that info blocks with prefered languages come first
+              const aIdx = preferedLanguages.indexOf(a.language.split('-')[0]);
+              const bIdx = preferedLanguages.indexOf(b.language.split('-')[0]);
+
+              return - aIdx + bIdx;
+            }
+          );
+          alert.alertInfos = orderedinfoBlocks;
+          return alert;
+        }
+      )
+    )
+  );
+}
 
 @Component({
   selector: 'app-warning-feed',
@@ -32,7 +55,8 @@ export class WarningFeedComponent {
     this.items = this.receivedWarningService.warnings().pipe(
       map(
         (items) => items.sort((a, b) => b.originatedAt.valueOf() - a.originatedAt.valueOf())
-      )
+      ),
+      orderByPreferredLanguages(environment.preferedLanguages)
     );
   }
 
